8af05063d34283370b756d1b8c3149a044a78134
Harshit Barnwal <harshitbaranwal407@gmail.com>
2025-06-20 23:59:23 +0530

    2 change

---
+++ Lacture-1.js
@@ -0,0 +1,6 @@
+function addsum(num1 , num2){
+    return num1+num2;
+}
+
+result=addsum(val1, val2);
+console.log(result);
---
+++ Sync,& async.js
@@ -0,0 +1,19 @@
+
+// *************Syncronus and Asynchronous Code in JavaScript ***************
+
+// Asynchronous Code in JavaScript allows certain operations to run in the background, enabling the program to continue executing other code without waiting for the operation to complete. This is particularly useful for tasks like network requests, file I/O, and timers.
+
+// console.log("starting the program...");
+
+// setTimeout(() => {  
+//     console.log("This is a delayed message after 2 seconds");
+// }, 2000);                                                  
+
+// console.log("This is a message after the delayed message");
+
+
+// synchronous Code
+
+console.log("This is a message 2");
+console.log("This is a message 2");
+console.log("This is a message 3 ");
