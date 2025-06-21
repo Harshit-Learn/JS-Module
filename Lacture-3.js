c5f3e2c0bde74d68510a56ad7ef2a39f9381f9ce
Harshit Barnwal <harshitbaranwal407@gmail.com>
2025-06-21 15:15:29 +0530

    Lacture-3 & html page

--- Lacture-3.js
+++ Lacture-3.js
@@ -1,10 +1,39 @@
-// In this Lacture we learn about Hoisting in JavaScript
-// Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of their containing scope during the compile phase. This means that you can use variables and functions before they are declared in the code, but only for function declarations, not for variable assignments.            
-//
-// console.log(myVar); // undefined     
-// var myVar = 5; // Variable declaration is hoisted, but assignment is not
-// console.log(myVar); // 5                                                                             
-//
-var a = 10;
-Console.log(a); // 10node
+// Hoisting in JavaScript
+// Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of their containing scope during the compilation phase. This means that you can use variables and functions before they are declared in the code, but only for function declarations, not for variable assignments.    
 
+// 1. Example of variable hoisting var
+// var x; // Variable declaration (hoisted)
+// var x = 10; // Variable declaration
+// console.log(x); // Outputs: 10
+
+
+// 2.let variable hoisting
+
+// Case 1: Using `let` before declaration (will throw an error)
+// let y; // Variable declaration (hoisted, but not initialized)       
+// y = 20; // Variable assignment
+// console.log(y); // Outputs: 20
+
+// console.log(y); // Outputs: 20
+// let y; // Variable declaration (hoisted, but not initialized)       
+// y = 20; // Variable assignment
+
+
+// 3.Example of function hoisting
+
+
+// heet(); // Outputs: This is a function declaration
+// function heet() {
+//     console.log("This is a function declaration");
+// }
+
+// // heet(); // Outputs: This is a function declaration
+
+// 4. Function with var Hoisting
+
+greet(); // Outputs: errror: greet is not defined
+var a = function greet(){
+    console.log("This is a function declaration with var");
+}
+
+// greet(); // Outputs: This is a function declaration with var
