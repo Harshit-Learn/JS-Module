af82375a5519ce42842f82e8d9558f4f689b4bea
Harshit Barnwal <harshitbaranwal407@gmail.com>
2025-06-21 19:16:01 +0530

    let/var/const concept

---
+++ Lacture-4.js
@@ -0,0 +1,26 @@
+// Var
+
+// var a=10; // initialize
+// var a=20; //Redeclare
+// console.log(a) // Outputs: 20
+
+
+// var a=10; // initialize
+//  a=20; //reassign
+// console.log(a) // Outputs: 20
+
+
+// ***** let **********
+
+
+let a=10; // initialize
+let a=20; //Redeclare
+console.log(a) // Outputs: 20
+
+
+// var a=10; // initialize
+//  a=20; //reassign
+// console.log(a) // Outputs: 20
+
+
+
