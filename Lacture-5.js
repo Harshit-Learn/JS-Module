5924e09a20355d3b62814bb67bb61fe53258c410
Harshit Barnwal <harshitbaranwal407@gmail.com>
2025-07-09 19:10:41 +0530

    function Vs Arrow function with this keyword

--- Lacture-5.js
+++ Lacture-5.js
@@ -20,14 +20,43 @@
 
 // **************** B. Arrow Function ****************************
 
-const greet = () =>{              // greet is the function name in Arrow Function
-    console.log("Hello harshit");
-} 
-greet(); // Outputs: Hello harshit
+// const greet = () =>{              // greet is the function name in Arrow Function
+//     console.log("Hello harshit");
+// } 
+// greet(); // Outputs: Hello harshit
 
 //  ************ Return somthing from arrow function ************************
-const abc = () => 'harshit'; {  //  return kra rhe he
 
-    const text = abc();
-    console.log(text); // Outputs: harshit
-}
+// const abc = () => 'harshit'; {  //  return kra rhe he
+
+//     const text = abc();
+//     console.log(text); // Outputs: harshit
+// }
+
+// ****************** "this keyword" in Arrow Function vs Traditional Function ****************************
+
+//  ************* with normal function ******************
+// const user = {
+//     name : " harsh",
+//     sayhi : function(){
+//         //console.log("Hey");
+//         //console.log("Hey" , name); // reference error: name is not defined
+//        console.log("Hey" , this.name ); // yha pr this user object ko refer kr rha he
+//     }
+// }
+// //console .log(user.name); // Outputs: harsh
+// user.sayhi();
+
+
+// *************** With arrow function ************
+
+// const user1 = {
+//     name: " harshit",
+//     sayhi : () =>{
+//         //console.log("Hey");  // outputs: Hey
+//           //console.log("Hey" , name); // ReferenceError: name is not defined
+//          console.log("Hey" , this.name); // Outputs: Hey undefined because  in arrow function "this keyword" does not refer to the object but to the global scope or the enclosing lexical context.
+//     } 
+// }
+
+// user1.sayhi();
