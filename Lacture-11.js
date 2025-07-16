2f97e98cd2aab4947d60ec0d8d5e73286ffe8753
Harshit Barnwal <harshitbaranwal407@gmail.com>
2025-07-16 11:50:29 +0530

    shallow & Deepcopy

---
+++ Lacture-11.js
@@ -0,0 +1,13 @@
+// Shallow & Deep copy
+// copy the Array & object 
+
+const orig={
+    name:"harshit",
+    address: {
+        city: "delhi"
+    }
+};
+
+console.log( orig.name);
+console.log( orig.address.city);
+
