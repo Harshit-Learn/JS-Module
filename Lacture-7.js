b8ca9744f59a3c82772841217817c995f21ec4e7
Harshit Barnwal <harshitbaranwal407@gmail.com>
2025-07-16 10:02:26 +0530

    chnage

--- Lacture-7.js
+++ Lacture-7.js
@@ -1,4 +1,4 @@
-// Optional Chaining , Nullish Coaescing  & Logical OR
+// 1. Optional Chaining , Nullish Coaescing  & Logical OR
 
 // const user = {                // object
 //     named: "Harsh",
@@ -27,12 +27,59 @@
 
 //usecase : In case of API call to fetch the data/properties agar result nahi atat he to browser pr faltu ka error deta he to solve these problem, we use (?)
 
-const user = {                // object
-    named: "Harsh",
+// const user = {                // object
+//     named: "Harsh",
 
-    prefernce : null
+//     prefernce : null
     
-};
+// };
 
-const city = user?.address?.city ; // you can use only with address but for safe zone we use for all
-console.log(city); // output: undefined
+// const city = user?.address?.city ; // you can use only with address but for safe zone we use for all
+// console.log(city); // output: undefined
+
+// ***********************************************************************************************************
+
+// 2. Nullish Coalesing
+
+// let x = null ?? "Default";
+// console.log(x); // Default
+
+// let y = undefined ?? "fallback";
+// console.log(y); // fallback
+
+// let z = " " ?? "Hello";
+// console.log(z); // " "  it is not null & not undefined
+// let a = 0 ?? 7;
+// console.log(a); // 0
+
+//***********************************************************************************
+
+// 3. Logical Operator
+// Falsy Value ( false , o , " " , nul , undefined)
+
+// let a= 4 || 4;
+// console.log(a); // 4
+
+// let b= false || "h1";
+// console.log(b); // h1
+
+// let c = 0 || "h2";
+// console.log(c); // h2
+
+// let d= null || "h3";
+// console.log(d); //h3
+
+let e= " " || "h4";
+console.log(e); // " " truthy
+let z = " " ?? "Hello";
+console.log(z); // " "
+
+// let f= undefined || "h5";
+// console.log(f);
+
+// let g= "hello" || "some";
+// console.log(g);
+
+
+
+
