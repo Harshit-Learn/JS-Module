bdba4cde6141440a3d777b7b9cb030ab45c75019
Harshit Barnwal <harshitbaranwal407@gmail.com>
2025-07-16 12:03:59 +0530

    shallow & Deepcopy

--- Lacture-11.js
+++ Lacture-11.js
@@ -1,13 +1,40 @@
 // Shallow & Deep copy
 // copy the Array & object 
 
+// shallow: JavaScript me jab hum kisi object ya array ko copy karte hain, hum do tarike se kar sakte hain:
+
 const orig={
     name:"harshit",
     address: {
         city: "delhi"
     }
 };
+//console.log( orig.name); // harshit
+//console.log( orig.address.city); // delhi
 
-console.log( orig.name);
-console.log( orig.address.city);
+// const copy = {... orig};
+// copy.name =  "amit";
+// copy.address.city = " mumbai";
+
+// console.log( orig.name); //     harshit
+// console.log( orig.address.city); // mumbai,  because  Agar object/array me nested object hai, wo reference se copy hota hai, alag memory nahi milti.
+
+// *******************************************************************************************************************
+
+// 2. Deep copy : Deep copy me nested object ya array bhi alag memory me copy ho jata hai, isliye original me koi change nahi hota.
+
+const original = {
+  name: "Harshit",
+  address: {
+    city: "Delhi"
+  }
+};
+
+const copy = JSON.parse(JSON.stringify(original));
+
+copy.name = "Amit";
+copy.address.city = "Mumbai";
+
+console.log(original.name); // Harshit ✅
+console.log(original.address.city); // Delhi ✅
 
