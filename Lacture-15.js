70fd869a32683c1843e979ba90e9c74b43a133a6
Harshit Barnwal <harshitbaranwal407@gmail.com>
2025-07-27 18:15:28 +0530

    add hoisting concept

--- Lacture-15.js
+++ Lacture-15.js
@@ -1,35 +1,35 @@
-//Ternary & short circuite operator (|| and &&)
+// //Ternary & short circuite operator (|| and &&)
 
-//1.Ternary : Concise the If- else . It is having 3 operand i> Condition ii> true iii> false
+// //1.Ternary : Concise the If- else . It is having 3 operand i> Condition ii> true iii> false
 
-const Age= 18;
-const vote = Age >18 ? "You are not eligible for the vote ": "You are eligible for the vote";
-console.log(vote);
-
-
-// 2. short circuite operator (|| and &&)
-
-// i> Logical OR(||) : 1st operand false then result second opernd & 2nd operand false the result 1st operand .    >> 1+0=1  or 0+1=1  . note : ||= addition
-
-const a = 5;
-const check = 5 || "harsh"
-console.log (check); // 5
-
-const check2 = 0 || 1;
-console.log(check2) // 1
-
-const check3 = null || "harsh ";
-console.log(check3) 
+// const Age= 18;
+// const vote = Age >18 ? "You are not eligible for the vote ": "You are eligible for the vote";
+// console.log(vote);
 
 
-// Logical AND (&&) : 1st operand false then result fist opernd & 2nd operand false the result 2nd operand . && treat as multiply.
+// // 2. short circuite operator (|| and &&)
 
-const a = 5;
-const check = 5 && "harsh"
-console.log (check); // harsh
+// // i> Logical OR(||) : 1st operand false then result second opernd & 2nd operand false the result 1st operand .    >> 1+0=1  or 0+1=1  . note : ||= addition
 
-const check2 = 0 && 1;
-console.log(check2) // 0
+// const a = 5;
+// const check = 5 || "harsh"
+// console.log (check); // 5
 
-const check3 = null && "harsh ";
-console.log(check3) ;// null
+// const check2 = 0 || 1;
+// console.log(check2) // 1
+
+// const check3 = null || "harsh ";
+// console.log(check3) 
+
+
+// // Logical AND (&&) : 1st operand false then result fist opernd & 2nd operand false the result 2nd operand . && treat as multiply.
+
+// const a = 5;
+// const check = 5 && "harsh"
+// console.log (check); // harsh
+
+// const check2 = 0 && 1;
+// console.log(check2) // 0
+
+// const check3 = null && "harsh ";
+// console.log(check3) ;// null
--- Lacture-4.js
+++ Lacture-4.js
@@ -1,5 +1,6 @@
-// Var
+// Var : Redeclare ,  reassign & Hoisted( resut: undefined).
 
+// console.log(a) // Outputs: undefined
 // var a=10; // initialize
 // var a=20; //Redeclare
 // console.log(a) // Outputs: 20
@@ -10,17 +11,23 @@
 // console.log(a) // Outputs: 20
 
 
-// ***** let **********
+// ***** let : Redeclare ( not) ,  reassign( yes) & Hoisted( resut: reference error due to TDZ).
 
 
 // let a=10; // initialize
 // let a=20; //Redeclare
-// console.log(a); // Outputs: 20
+// console.log(a); // Outputs: erroe
 
 
-// var a=10; // initialize
+// let  a=10; // initialize
 //  a=20; //reassign
 // console.log(a) // Outputs: 20
 
+// const  : Redeclare ( not) ,  reassign( not) & Hoisted( resut: reference error due to TDZ).
+
+// const a = 6;
+// //const a = 8; // error 
+// a = 8 //  Assignment to constant variable.
+// console.log(a)
 
 
